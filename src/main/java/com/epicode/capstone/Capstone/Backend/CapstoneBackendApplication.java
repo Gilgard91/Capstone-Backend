de4add91a3247a08e21a359e7c7f97dd487b8b4c
Giordano <zagattigiordano@hotmail.it>
2024-03-26 22:27:13 +0100

    commit

--- src/main/java/com/epicode/capstone/Capstone/Backend/CapstoneBackendApplication.java
+++ src/main/java/com/epicode/capstone/Capstone/Backend/CapstoneBackendApplication.java
@@ -11,21 +11,6 @@ public class CapstoneBackendApplication {
 	public static void main(String[] args) throws NoSuchAlgorithmException {
 		SpringApplication.run(CapstoneBackendApplication.class, args);
 
-
-//		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-//
-//
-//		keyPairGenerator.initialize(2048);
-//
-//
-//		KeyPair keyPair = keyPairGenerator.generateKeyPair();
-//
-//
-//		PublicKey publicKey = keyPair.getPublic();
-//		PrivateKey privateKey = keyPair.getPrivate();
-//
-//		System.out.println("Chiave pubblica: " + publicKey);
-//		System.out.println("Chiave privata: " + privateKey);
 	}
 
 }
